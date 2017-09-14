bda74edb976cae7abfde65e4d329a6e32f0d2c77
Dave Townsend <dtownsend@oxymoronical.com>
2017-09-13 19:46:11 -0700

    More styling work and remove dependency on stop list in popup.

--- content/popup.js
+++ content/popup.js
@@ -1,17 +1,3 @@
-const STOPS = [9299, 7500];
-
-function init() {
-  let list = document.getElementById("stopList");
-
-  for (let stop of STOPS) {
-    let row = document.createElement("tr");
-    row.innerHTML = "<td colspan=\"4\" class=\"loading\"></td>";
-    list.appendChild(row);
-  }
-}
-
-window.addEventListener("load", init, { once: true });
-
 let port = browser.runtime.connect(null, { name: "panel" });
 
 window.addEventListener("unload", () => {
@@ -35,11 +21,16 @@ port.onMessage.addListener((message) => {
       for (let i = 0; i < 3; i++) {
         let arrival = document.createElement("td");
         if (i < stop.arrivals.length) {
-          let time = stop.arrivals[i].expected ? stop.arrivals[i].expected : stop.arrivals[i].scheduled;
+          let time = stop.arrivals[i].estimated ? stop.arrivals[i].estimated : stop.arrivals[i].scheduled;
           let diff = time.getTime() - Date.now();
           diff /= 60000;
-          arrival.innerText = `${parseInt(diff)}mins`;
-          arrival.className = stop.arrivals[i].expected ? "arrival expected" : "arrival scheduled";
+
+          let span = document.createElement("span");
+          span.className = stop.arrivals[i].estimated ? "estimated" : "scheduled";
+          span.innerText = `${parseInt(diff)}mins`;
+
+          arrival.className = "arrival";
+          arrival.appendChild(span);
         }
         row.appendChild(arrival);
       }
