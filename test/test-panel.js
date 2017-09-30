828122f6777710e26e940159178c90d38d3d8863
Dave Townsend <dtownsend@oxymoronical.com>
2017-09-30 12:42:25 -0700

    Add more Stop tests.

--- test/test-panel.js
+++ test/test-panel.js
@@ -21,5 +21,18 @@ describe("Panel Stop component", function() {
     expect(wrapper.find("td")).to.have.length(4);
     expect(wrapper.find("Arrival")).to.have.length(1);
     expect(wrapper.find("Arrival").first().prop("arrival")).to.deep.equal({ estimated: 5, scheduled: 6 });
+
+    stop.arrivals.push({
+      estimated: null,
+      scheduled: 7,
+    });
+
+    wrapper = shallow(<Stop stop={stop} />);
+    expect(wrapper.find(".routeName").html()).to.equal("<p class=\"routeName\">route</p>");
+    expect(wrapper.find(".stopName").html()).to.equal("<p class=\"stopName\">stop</p>");
+    expect(wrapper.find("td")).to.have.length(3);
+    expect(wrapper.find("Arrival")).to.have.length(2);
+    expect(wrapper.find("Arrival").first().prop("arrival")).to.deep.equal({ estimated: 5, scheduled: 6 });
+    expect(wrapper.find("Arrival").at(1).prop("arrival")).to.deep.equal({ estimated: null, scheduled: 7 });
   });
 });
