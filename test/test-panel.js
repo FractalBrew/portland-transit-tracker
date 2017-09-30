f65d7691ba6d5c46e15c8403fe24fff8bb63db58
Dave Townsend <dtownsend@oxymoronical.com>
2017-09-30 13:06:43 -0700

    Add tests for removing stops.

--- test/test-panel.js
+++ test/test-panel.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { expect } from "chai";
 import { shallow } from "./utils";
+import sinon from "sinon";
 
 import Stop from "../src/panel/stop.jsx";
 
@@ -34,5 +35,35 @@ describe("Panel Stop component", function() {
     expect(wrapper.find("Arrival")).to.have.length(2);
     expect(wrapper.find("Arrival").first().prop("arrival")).to.deep.equal({ estimated: 5, scheduled: 6 });
     expect(wrapper.find("Arrival").at(1).prop("arrival")).to.deep.equal({ estimated: null, scheduled: 7 });
+  });
+
+  it("Clicking remove should send a message", function() {
+    const port = {
+      postMessage: sinon.spy(),
+    };
+
+    const stop = {
+      route: 5,
+      routeName: "route",
+      direction: 7,
+      stop: 10,
+      stopName: "stop",
+      arrivals: [{
+        estimated: 5,
+        scheduled: 6,
+      }],
+    };
+
+    let wrapper = shallow(<Stop port={port} stop={stop} />);
+    wrapper.find(".delete").simulate("click");
+    expect(port.postMessage.calledOnce).to.be.true;
+    expect(port.postMessage.args[0][0]).to.deep.equal({
+      message: "removeStop",
+      data: {
+        route: 5,
+        direction: 7,
+        stop: 10,
+      },
+    });
   });
 });
